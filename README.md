4a242ae763d3a7fb09177794294a3dd34efb0d98
dongwenxiao <cs_victor@126.com>
2017-11-01 15:30:55 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # sp-mysql
-Operate mysql for sp-bsae
+super-project moudle for mysql operate.
+
+
+# Usage
+
+## Init
+
+```js
+import spMysql from 'sp-mysql'
+
+const config = {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'xxxx',
+    password: 'xxxx',
+    database: 'db_name'
+}
+
+const mysql = new spMysql(config)
+
+
+```
+
+## Query
+
+```js
+
+
+let sql = 'SELECT 1 + 1 AS Test'
+let [result] = await mysql.query(sql)
+
+// result 查询结果并保护下面数据
+// { affectedRows, insertId } = result
+
+```
+
+
+## Log
+
+```js
+mysql.openLog() // open log
+
+mysql.closeLog() // close log
+```
+
+## Test Connection
+
+```js
+mysql.testConnect()
+
+// console => MySQL [db_name] connected √
+//         => MySQL connect failed X
+```
--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-import Sequelize from 'sequelize'
+const mysql = require('mysql2/promise')
 
 export default class spMysql {
-    constructor() {
-        var sequelize = new Sequelize('ux_translate_platform', 'root', '', {
+
+    constructor(opt) {
+        this.opt = Object.assign({
+            connectionLimit: 10,
             host: '127.0.0.1',
-            dialect: 'mysql',
-
-            pool: {
-                max: 5,
-                min: 0,
-                idle: 10000
-            },
-
-            // SQLite only
-            // storage: 'path/to/database.sqlite'
-        })
-
-        var User = sequelize.define('test_user', {
-            firstName: {
-                type: Sequelize.STRING
-            },
-            lastName: {
-                type: Sequelize.STRING
-            }
-        });
-
-        // force: true will drop the table if it already exists
-        User.sync({ force: false }).then(function() {
-            // Table created
-            return User.create({
-                firstName: 'John',
-                lastName: 'Hancock'
-            });
-        }).then(() => {
-
-            User.findAll().then(function(users) {
-                console.log(users)
-            })
-        })
-
+            port: '3306',
+            // user: 'root',
+            // password: 'root',
+            database: 'no-set-db-name',
+            charset:'UTF8_GENERAL_CI'
+        }, opt)
+        this.instance = this.createPool(opt)
+        this.testConnect()
+        this._openLog = false
     }
+
+
+    createPool() {
+        return mysql.createPool(this.opt)
+    }
+
+    testConnect() {
+        this.instance.query('SELECT 1 + 1 AS Test')
+            .then(() => {
+                console.info(`MySQL [${this.opt.database}] connected √`)
+            })
+            .catch((err) => {
+                console.error('MySQL connect failed X')
+                console.error(err)
+            })
+    }
+
+    openLog() {
+        this._openLog = true
+    }
+
+    closeLog() {
+        this._openLog = false
+    }
+
+    query(sql, params) {
+        if (this._openLog) {
+            console.log(`SQL: ${sql}`)
+            if (params) console.log(`params: ${JSON.stringify(params)}`)
+        }
+
+        return this.instance.query(sql, params)
+    }
+
 }
